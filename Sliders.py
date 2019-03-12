a35f9e2fef2fe1c7da94be1959db3185a85b026b
RWitte <robertwitte.p@gmail.com>
2019-03-12 15:25:26 +0100

    Update Sliders.py

--- Sliders.py
+++ Sliders.py
@@ -1,21 +1,37 @@
 # -*- coding: utf-8 -*-
 """
-Sliding function for subsampling images
+Spyder Editor
+
+This is a temporary script file.
 """
 
 import numpy as np
 import cv2
-import os
+import glob as glob
 
-### Input folder, replace "\" with "/", "r" for raw string
+### Input folder, "r" for raw string, , load in .TIF files, replace "\" with "/"
 folder = r"C:\Users\Robert\Desktop\labdust-gan\151130-AY-artifacts-4x-dapi-gfp-tritc-cy5_Plate_1935\TimePoint_1"
+folder += r"\*.TIF"
+folder = folder.replace("\\", "/")
 
-def load_images_from_folder(folder):
-    images = []
-    for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename,),-1)
-        if img is not None:
-            images.append(img)
-    return images
+images = []
+files = glob.iglob(folder)
+for myFile in files:
+    image = cv2.imread(myFile, -1)
+    images.append(image)
 
-Images = load_images_from_folder(folder)
+#print('Array shape:', np.array(images).shape)
+
+#Convert list of arrays into 3D np.array
+
+###x = np.zeros((np.array(images).shape[0],np.array(images).shape[1],np.array(images).shape[2]))
+
+images = np.array(images)
+
+"""
+###Look at images
+from matplotlib import pyplot as plt
+plt.imshow(images[0], interpolation='nearest')
+plt.show()
+"""
+
