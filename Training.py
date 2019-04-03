82fb9e1745a6d1e0f6524614e63eaa6f89b54ec3
RWitte <robertwitte.p@gmail.com>
2019-04-03 15:16:31 +0200

    Update Training.py

--- Training.py
+++ Training.py
@@ -7,14 +7,11 @@ from IPython.display import Image, SVG
 import matplotlib.pyplot as plt
 
 import numpy as np
-import keras
-from keras.datasets import mnist
 from keras.models import Model, Sequential
+from keras.optimizers import RMSprop
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Reshape, Activation, Dropout
-from keras import regularizers
 from sklearn.model_selection import train_test_split
 from keras import backend as K
-import tensorflow as tf
 
 K.clear_session()
 
@@ -27,196 +24,71 @@ K.clear_session()
 
 folder = r"C:\Users\RobertWi\Desktop\151130-AY-artifacts-10x-dapi-gfp-tritc-cy5_Plate_1934\TimePoint_1"
 crop_to = 1000
-x_len = 100
-y_len = 100
+x_len = 101
+y_len = 101
 
 dataset = sliders(folder, crop_to, x_len, y_len,2)
 
 #Add channel dimensions, 3D version
-def datasetRGB(x):
-    x_empty = np.empty((x.shape[0], x.shape[1], x.shape[2], 3), dtype=np.uint8)
-    x_empty[:, :, :, 2] = x_empty[:, :, :, 1] = x_empty[:, :, :, 0] = x
-    return(x_empty)
-    
-dataset = datasetRGB(dataset)
+#def datasetRGB(x):
+#    x_empty = np.empty((x.shape[0], x.shape[1], x.shape[2], 3), dtype=np.uint8)
+#    x_empty[:, :, :, 2] = x_empty[:, :, :, 1] = x_empty[:, :, :, 0] = x
+#    return(x_empty)
+#    
+#dataset = datasetRGB(dataset)
 
 #1D version
-#dataset = np.reshape(dataset, (dataset.shape[0], dataset.shape[1], dataset.shape[2], 1))
+dataset = np.reshape(dataset, (dataset.shape[0], dataset.shape[1], dataset.shape[2], 1))
 
 #Normalization to [0,1]
-dataset = ((dataset-np.min(dataset))/np.ptp(dataset)).astype('float16')
+dataset = ((dataset-np.min(dataset))/np.ptp(dataset)).astype('float32')
 x_train, x_test = train_test_split(dataset, test_size=0.2, random_state=1)
 
-#x_train = x_train.reshape(-1,(len(x_train), np.prod(x_train.shape[1:])),1)
-#x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-#input_shape = x_train.shape[1]
-#print((x_train.shape, x_test.shape))
+batch_size = 32
+epochs = 50
+inChannel = 1
+x, y = 100, 100
+input_img = Input(shape = (x, y, inChannel))
 
+#def autoencoder(input_img):
+#    #encoder
+#    #input = 28 x 28 x 1 (wide and thin)
+#    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 32
+#    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
+#    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
+#    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
+#    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
+#
+#    #decoder
+#    conv4 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3) #7 x 7 x 128
+#    up1 = UpSampling2D((2,2))(conv4) # 14 x 14 x 128
+#    conv5 = Conv2D(64, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 64
+#    up2 = UpSampling2D((2,2))(conv5) # 28 x 28 x 64
+#    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
+#    return decoded
+#
+#autoencoder = Model(input_img, autoencoder(input_img))
+#autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
+#autoencoder.summary()
+#
+#autoencoder_train = autoencoder.fit(x_train, x_train, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, x_test))
+
+#
 model = Sequential()
+###Encoder Layer
+model.add(Conv2D(1, (32,32),data_format='channels_last', activation ="relu", padding='same', input_shape=(101,101,1)))
+model.add(Conv2D(32, (32,32),data_format='channels_last', activation ="relu", padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2),))
+#model.add(Conv2D(64, (3, 3),data_format='channels_last', activation ="relu", padding='same',))
+#model.add(MaxPooling2D(pool_size=(2, 2),))
+####Decoder Layer
+#model.add(Conv2D(32, (3, 3),data_format='channels_last', activation ="relu", padding='same',))
+#model.add(UpSampling2D((2,2)))
+#model.add(Conv2D(1, (3,3), activation = 'sigmoid', padding = 'same'))
 
-#Encoder Layer
-model.add(Conv2D(64, (3, 3),data_format='channels_last', padding='same',))
-#model.add(MaxPooling2D(pool_size=(2, 2), input_shape=(32,32,1)))
-#model.add(Dropout(0.25))
-#model.add(Flatten())
-#
-#model.add(Dense((4*encoding_dim), activation = 'relu'))
-#model.add(Dense((2*encoding_dim), activation = 'relu'))
-#model.add(Dense((encoding_dim), activation = 'relu'))
-#
-##Decoder Layer
-#
-#model.add(Dense((2*encoding_dim), activation='relu'))
-#model.add(Dense((4*encoding_dim), activation='relu'))
-#model.add(Dense(10000, activation='sigmoid'))
-#
-#model.summary()
-#
-#
-#input_img = Input(shape=(2500,))
-#encoder_layer1 = model.layers[4]
-#encoder_layer2 = model.layers[5]
-#encoder_layer3 = model.layers[6]
-#encoder = Model(input_img, encoder_layer3(encoder_layer2(encoder_layer1(input_img))))
-#
-#encoder.summary()
-#
-model.compile(optimizer='adam', loss='mean_squared_error')
+model.compile(optimizer='RMSprop', loss='mean_squared_error')
+
 model.fit(x_train, x_train,
                 epochs=50,
-                batch_size=256,
+                batch_size=32,
                 validation_data=(x_test, x_test))
-
-
-### Single Lay Encoder starts here
-""" 
-
-autoencoder = Sequential()
-autoencoder.add(
-    Dense(encoding_dim, input_shape=(input_dim,), activation='relu')
-)
-autoencoder.add(
-    Dense(input_dim, activation='sigmoid')
-)
-
-autoencoder.summary()
-
-input_img = Input(shape=(input_dim,))
-encoder_layer = autoencoder.layers[0]
-encoder = Model(input_img, encoder_layer(input_img))
-
-encoder.summary()
-
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-autoencoder.fit(x_train, x_train,
-                epochs=20,
-                batch_size=100,
-                shuffle=True,
-                validation_data=(x_test, x_test))
-
-### Plot Results
-
-num_images = 10
-np.random.seed(42)
-random_test_images = np.random.randint(x_test.shape[0], size=num_images)
-
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = autoencoder.predict(x_test)
-
-plt.figure(figsize=(18, 4))
-
-for i, image_idx in enumerate(random_test_images):
-    # plot original image
-    ax = plt.subplot(3, num_images, i + 1)
-    plt.imshow(x_test[image_idx].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-    
-    # plot encoded image
-    ax = plt.subplot(3, num_images, num_images + i + 1)
-    plt.imshow(encoded_imgs[image_idx].reshape(8, 4))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-    # plot reconstructed image
-    ax = plt.subplot(3, num_images, 2*num_images + i + 1)
-    plt.imshow(decoded_imgs[image_idx].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
-
-"""
-### Single Layer Encoder ends here
-"""
-### Deep Autoencoder starts here
-
-autoencoder = Sequential()
-
-# Encoder Layers
-autoencoder.add(Dense(4 * encoding_dim, input_shape=(input_dim,), activation='relu'))
-autoencoder.add(Dense(2 * encoding_dim, activation='relu'))
-autoencoder.add(Dense(encoding_dim, activation='relu'))
-
-# Decoder Layers
-autoencoder.add(Dense(2 * encoding_dim, activation='relu'))
-autoencoder.add(Dense(4 * encoding_dim, activation='relu'))
-autoencoder.add(Dense(input_dim, activation='sigmoid'))
-
-autoencoder.summary()
-
-
-input_img = Input(shape=(input_dim,))
-encoder_layer1 = autoencoder.layers[0]
-encoder_layer2 = autoencoder.layers[1]
-encoder_layer3 = autoencoder.layers[2]
-encoder = Model(input_img, encoder_layer3(encoder_layer2(encoder_layer1(input_img))))
-
-encoder.summary()
-
-
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-autoencoder.fit(x_train, x_train,
-                epochs=10,
-                batch_size=500,
-                validation_data=(x_test, x_test))
-
-### Plot DA results
-
-num_images = 10
-np.random.seed(42)
-random_test_images = np.random.randint(x_test.shape[0], size=num_images)
-
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = autoencoder.predict(x_test)
-
-plt.figure(figsize=(18, 4))
-
-for i, image_idx in enumerate(random_test_images):
-    # plot original image
-    ax = plt.subplot(3, num_images, i + 1)
-#    plt.imshow(x_test[image_idx].reshape(np.array(dataset_norm).shape[1], np.array(dataset_norm).shape[2]))
-    plt.imshow(x_test[image_idx].reshape(100, 100))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-    
-    # plot encoded image
-    ax = plt.subplot(3, num_images, num_images + i + 1)
-    plt.imshow(encoded_imgs[image_idx].reshape(int((encoding_dim**0.5)), int((encoding_dim**0.5))))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-    # plot reconstructed image
-    ax = plt.subplot(3, num_images, 2*num_images + i + 1)
-    plt.imshow(decoded_imgs[image_idx].reshape(np.array(dataset_norm).shape[1], np.array(dataset_norm).shape[2]))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
-
-### Deep Autoencoder ends here
-"""
